ed7212fbfdcd62aeae1dc0cf49628a91d4dec635
Rushan Qazi <rushanqazi@Rushans-MacBook-Air.local>
2023-12-11 10:38:06 +1100

    feat: Layout for Register Page with working matching passwords. Error blocks not working with ngif

--- Documents/Programming/social-sphere/src/app/components/register/register.component.ts
+++ Documents/Programming/social-sphere/src/app/components/register/register.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {RouterLink} from "@angular/router";
+import {passwordMatchValidator} from "../../shared/password-match-directive";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    CardModule,
+    FormsModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
+  registerForm = this.fb.group({
+    fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {
+      validators: passwordMatchValidator
+  })
+  constructor(private fb: FormBuilder) {}
+  get fullName() {return this.registerForm.controls['fullName'];}
+  get email() {return this.registerForm.controls['email'];}
+  get password() {return this.registerForm.controls['password']}
+  get confirmPassword() {return this.registerForm.controls["confirmPassword"]}
 }
